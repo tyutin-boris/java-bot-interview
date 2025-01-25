ef79c60aa24cc079f347ce46f6305e4bd8f05253
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2025-01-25 20:25:01 +0300

    init commit

---
+++ src/main/java/ru/boris/demo/tg/bot/DemoTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.boris.demo.tg.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/boris/demo/tg/bot/DemoTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boris.demo.tg.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
